5f4066f8a8d7ddfbdea625c5e019c347de6b00e8
marcoigorr <petronio.mrc@gmail.com>
2022-10-12 18:13:35 +0200

    updated fear inoculum style

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ for (let i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', function(e) {        
         switch (i) {
             case 0: // Fear Inoculum
-                window.open('../html/FearInoculum.html', '_blank');  
+                window.open('../html/FearInoculum.html', '_self');  
                 break;
             case 1: // Lateralus
-                window.open('../html/Lateralus.html', '_blank');
+                window.open('../html/Lateralus.html', '_self');
                 break;
             case 2: // 10.000 Days
-                window.open('../html/10000Days.html', '_blank');
+                window.open('../html/10000Days.html', '_self');
                 break;
             case 3: // Opitae
-                window.open('../html/Opitae.html', '_blank');
+                window.open('../html/Opitae.html', '_self');
                 break;
         }
     });
