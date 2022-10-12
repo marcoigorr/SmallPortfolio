c921360db2f730a6fa94cd0f24082f1d34822e44
marcoigorr <petronio.mrc@gmail.com>
2022-10-12 12:46:04 +0200

    Added vinyls pages

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ for (let i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', function(e) {        
         switch (i) {
             case 0: // Fear Inoculum
-                window.open('https://example.com', '_blank');  
+                window.open('../html/FearInoculum.html', '_blank');  
                 break;
             case 1: // Lateralus
-                window.open('https://example.com', '_blank');
+                window.open('../html/Lateralus.html', '_blank');
                 break;
             case 2: // 10.000 Days
-                window.open('https://example.com', '_blank');
+                window.open('../html/10000Days.html', '_blank');
                 break;
             case 3: // Opitae
-                window.open('https://example.com', '_blank');
+                window.open('../html/Opitae.html', '_blank');
                 break;
         }
     });
