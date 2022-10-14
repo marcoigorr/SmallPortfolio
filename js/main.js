637d06200b2c81cc7d7a48170225b2c356813d9a
marcoigorr <petronio.mrc@gmail.com>
2022-10-14 15:28:59 +0200

    Changed index 4 divs with table for ferrari

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 
-const divs = document.querySelectorAll('.link');
+const table_elem = document.querySelectorAll('td');
 
-for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', function(e) {        
+for (let i = 0; i < table_elem.length; i++) {
+    table_elem[i].addEventListener('click', function(e) {        
         switch (i) {
             case 0: // Fear Inoculum
                 window.open('\html/FearInoculum.html', '_self');  
                 break;
             case 1: // Lateralus
-                window.open('\html/Lateralus.html', '_self');
+                window.open('\html/10000Days.html', '_self');
                 break;
             case 2: // 10.000 Days
-                window.open('\html/10000Days.html', '_self');
+                window.open('\html/Lateralus.html', '_self');
                 break;
             case 3: // Opitae
                 window.open('\html/Opitae.html', '_self');
